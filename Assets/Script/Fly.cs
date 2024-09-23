1b882aa3fa0f6e14ced2da75cb597e448168d1bd
Glenjamin120 <126250651+Glenjamin120@users.noreply.github.com>
2024-09-23 16:51:42 +0100

    Flapping v.1

--- Assets/Script/Fly.cs
+++ Assets/Script/Fly.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Fly : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float _velocity = 1.5f;
+    private Rigidbody2D _rb;
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _rb.velocity = Vector2.up * _velocity;
+        }
     }
 }
